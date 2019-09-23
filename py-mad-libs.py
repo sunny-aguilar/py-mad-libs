d6a23e0b6bbf697f0c3b76f25c1bec65fdf86d4f
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-22 20:25:04 -0700

    libs

--- py-mad-libs.py
+++ py-mad-libs.py
@@ -1,10 +1,10 @@
 # This simple project creates a phrased based on user's input
 
 words = ['cat', 'balcony']
-sentence = ['The', ' jumped of the ' , '.']
+sentence = ['The', ' jumped of the', '.']
 phrase = ''
 
-for i in range(len(sentence))
+for i in range(len(sentence)):
     phrase += sentence[i]
 
 print(phrase)
